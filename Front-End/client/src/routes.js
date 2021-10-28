379ce8e5af65156850c76cbcc6a704951d2cc25e
gabriel <gabriel.collo@rocky.ag>
2021-10-28 09:30:16 -0300

    Feat: Adiciona Routes

--- Front-End/client/src/routes.js
+++ Front-End/client/src/routes.js
@@ -3,6 +3,7 @@ import {Switch, Route, BrowserRouter,useHistory } from 'react-router-dom';
 
 import Login from "./pages/Login"
 import Home from "./pages/Home"
+import Content from "./pages/Content"
 
 export default function Routes(){
     return(
@@ -10,6 +11,7 @@ export default function Routes(){
             <Switch>
                 <Route path="/" exact component={Login} />
                 <Route path="/home" component={Home} />
+                <Route path="/content" component={Content} />
             </Switch> 
         </BrowserRouter>
     )
